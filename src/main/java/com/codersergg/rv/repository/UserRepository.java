d0429fcc37391bb9a8e9f623fe5ce8b748322699
CoderSergg <ShopinSergg@gmail.com>
2021-09-11 14:19:27 +0300

    refactoring POM

--- src/main/java/com/codersergg/rv/repository/UserRepository.java
+++ src/main/java/com/codersergg/rv/repository/UserRepository.java
@@ -4,13 +4,11 @@ import com.codersergg.rv.model.User;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -19,11 +17,9 @@ import java.util.Optional;
 @Tag(name = "User Controller")
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @RestResource(rel = "by-email", path = "by-email")
     @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
     Optional<User> findByEmailIgnoreCase(String email);
 
-    @RestResource(rel = "by-lastname", path = "by-lastname")
     Page<User> findByLastNameContainingIgnoreCase(String lastName, Pageable page);
 
     @Override
